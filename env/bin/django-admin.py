97ff2e56d67c693e5fc9cf66a011031a9a347726
NishithD <nishithdasari100@gmail.com>
2022-05-28 16:02:55 +0530

    modify

--- env/bin/django-admin.py
+++ env/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/nishith/Desktop/ms-engage22/env/bin/python3
+#!/home/nishith/Desktop/Attendance-Management-System-Using-Face-Recognition/Attendance-System-Using-Face-Recognition/env/bin/python3
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
